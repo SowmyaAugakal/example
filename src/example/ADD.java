b46d013dd025368e4c0e10e99a7bff1cd9f86502
sowmya <sowmy@LAPTOP-6TEFR132>
2023-11-28 22:46:43 +0530

    ADD TWO NUMBERS

---
+++ src/example/ADD.java
@@ -0,0 +1,14 @@
+package example;
+
+public class ADD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		int y=6;
+		int sum=x+y;
+		System.out.println(sum);
+
+	}
+
+}
